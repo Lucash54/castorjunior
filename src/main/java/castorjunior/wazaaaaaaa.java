f8d95da973c28c42a81256a53552205b727c604d
lucas <lucas@zbook-29>
2018-01-09 14:31:13 +0100

    wazaaa

---
+++ src/main/java/castorjunior/wazaaaaaaa.java
@@ -0,0 +1,25 @@
+package castorjunior;
+
+
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+ 
+import java.io.File;
+
+
+
+
+
+public class wazaaaaaaa {
+	
+	
+	public void foo() {
+	CSVLoader loader = new CSVLoader();
+   // loader.setSource();
+    //Instances data = loader.getDataSet();
+	}
+    
+    
+
+}
