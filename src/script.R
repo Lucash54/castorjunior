f53277f997df54d56c1fc88af999f81f9b7dc77b
luc <luc@luc-HP-ZBook-15>
2018-01-16 18:00:56 +0100

    rengine qui marche

--- src/script.R
+++ src/script.R
@@ -1,4 +1,8 @@
-library("org.renjin.cran.tree")
+library('org.renjin.cran:randomForest')
+"la librairie randomForest est chargée"
 iris <- read.csv("src/iris.csv")
-arbre <- tree(Species~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,data=iris)
-3+3
+"les données des iris sont chargées"
+arbre <- randomForest(Species~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,data=iris)
+total <- length(iris$Species)
+conf <- arbre$confusion[,1:3]
+accuracy <- (conf[1,1] + conf[2,2] + conf[3,3])/total
