c43966e7e6e2894b99f97b528ab417a6f0d66229
luc <luc@luc-HP-ZBook-15>
2018-02-26 16:29:27 +0100

    Ajout du second csv et amélioration de Rengine

--- src/main/java/castorjunior/Rengine.java
+++ src/main/java/castorjunior/Rengine.java
@@ -4,29 +4,70 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 import javax.script.*;
 import org.renjin.script.*;
 
 public class Rengine {
 
-	  @SuppressWarnings("restriction")
+	 @SuppressWarnings("restriction")
 	public static void main(String[] args) throws Exception {
 		
-	    // create a script engine manager:
+		//On crée un scanner, pour que l'utilisateur puisse donner l'adresse du csv, et le nom de la variable explicative
+		
+		Scanner saisieUtilisateur = new Scanner(System.in); 
+		
+		//On crée un scanner, pour que l'utilisateur puisse donner l'adresse du csv,
+		// le nom de la variable explicative
+		// le nombre d'arbres
+		
+		System.out.println("Veuillez saisir l'adresse du csv :");
+		//String strAdresse = saisieUtilisateur.next();
+		//J'ai remplacé par une adresse existante pour ne pas taper à chaque fois, il faudra décommenter à la fin
+		String strAdresse = "/home/luc/git/castorjunior/src/pages.csv";
+		
+		System.out.println("Veuillez saisir le nom de la variable à expliquer :");
+		//String strVariable = saisieUtilisateur.next();
+		//J'ai remplacé par la variable, il faudra décommenter à la fin
+		String strVariable = "nbPages";
+		
+		System.out.println("Veuillez saisir le nombre d'arbres utilisés :");
+		//String nbArbre = saisieUtilisateur.next();
+		//J'ai remplacé par la variable, il faudra décommenter à la fin
+		String nbArbre = "800";
+		
+		
+	    // crée un script Rengine manager:
 	    RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
 	    
-	    // create a Renjin engine:
+	    // crée un Rengine:
 		ScriptEngine engine = factory.getScriptEngine();
 
 	    
-	    try{	    		    	
-	    	// on crée d'abord un lecteur de fichier r pour lire le fichier ligne à ligne 
+	    try{
 	    	
-	    	InputStream flux = new FileInputStream("src/script.R"); 
+	    	// on importe d'abord le nom de l'adresse mémoire du csv sous R
+	    	engine.eval("csv <- read.csv("+"\""+strAdresse+"\")");
+	    	System.out.println("Import des données réussi!");
+	    	
+	    	// puis le nom de la variable
+	    	engine.eval("nomVar <- "+"\""+strVariable+"\"");
+	    	
+	    	// puis le nombre d'arbres utilisés
+	    	engine.eval("nbArbre <- as.numeric("+nbArbre+")");
+	    	
+	    	// puis on crée  un lecteur de fichier r pour lire le script du randomForest ligne à ligne 
+	    	
+	    	InputStream flux = new FileInputStream("src/script.R");
 	    	InputStreamReader lecture = new InputStreamReader(flux);
 	    	BufferedReader buff = new BufferedReader(lecture);
 	    	String ligne;
+	    	
+	    	// exemple des pages : /home/luc/git/castorjunior/src/pages.csv
+	    	// nbPages comme nom de variable
+	    	// exemple des iris : /home/luc/git/castorjunior/src/iris.csv
+	    	// Species comme nom de variable
 	    	
 	    	// tant que le fichier n'est pas entièrement lu, càd qu'on arrive pas à la dernière ligne
 	    	
--- src/script.R
+++ src/script.R
@@ -1,8 +1,24 @@
 library('org.renjin.cran:randomForest')
-"la librairie randomForest est chargée"
-iris <- read.csv("src/iris.csv")
-"les données des iris sont chargées"
-arbre <- randomForest(Species~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,data=iris)
-total <- length(iris$Species)
-conf <- arbre$confusion[,1:3]
-accuracy <- (conf[1,1] + conf[2,2] + conf[3,3])/total
+"Librairie randomForest chargée!"
+"Nombre d'arbres utilisés:"
+nbArbre
+"On regarde le nom des colonnes" 
+col <- colnames(csv)
+"La variable à expliquer est (s'il n'y a rien après cette ligne, la variable n'existe pas, il faudra checker le csv)"
+colnames(csv)[which(colnames(csv)==nomVar)]
+"On change le nom de la variable à expliquer en var"
+colnames(csv)[which(colnames(csv)==nomVar)]="var"
+"On transforme la variable à expliquer en facteur"
+csv$var <- as.factor(as.character(csv$var))
+"On lance l'arbre"
+arbre <- randomForest(var~., data=csv, ntree=nbArbre)
+"Puis on calcule l'accuracy"
+taille <- length(levels(csv$var))
+total <- length(csv$var)
+conf <- arbre$confusion[,1:taille]
+"On l'initialise la précision à 0"
+accuracy <- 0
+"La précision vaut la trace de la matrice de confusion divisée par le nombre d'individus classés"
+for(i in 1:taille){accuracy <- accuracy + conf[i,i]/total}
+"accuracy = "
+accuracy
