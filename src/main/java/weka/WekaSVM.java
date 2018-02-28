b8f8b09b06b8327179761849146a8083cc251682
luc <luc@luc-HP-ZBook-15>
2018-02-28 10:46:04 +0100

    weka

--- src/main/java/weka/WekaSVM.java
+++ src/main/java/weka/WekaSVM.java
@@ -1,18 +1,15 @@
 package weka;
- 
-import java.awt.BorderLayout;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Random;
-import javax.swing.JFrame;
 
-import weka.classifiers.*;
-import weka.classifiers.trees.J48;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.SMO;
 import weka.core.Instances;
-import weka.gui.treevisualizer.PlaceNode2;
-import weka.gui.treevisualizer.TreeVisualizer;
 
-public class WekaTraining {
+public class WekaSVM {
+
 	
 	/** à faire disparaitre dès qu'on aura les menus */
 	
@@ -25,22 +22,21 @@ public class WekaTraining {
 		
 		// On lance le training 
 		training("./src/pages.arff","nbPages");
-		//training("./src/iris.arff","Species");
+		training("./src/iris.arff","Species");
 	}
 	
-	/** gère la classification par arbre CART
+	/** gère la classification par svm
 	 * en entrée :
 	 * - le chemin du fichier .arff
 	 * - le nom de la variable à expliquer dans le csv
 	 * en sortie console:
-	 * - le résumé du modèle CART utilisé, comprenant la précision du modèle
-	 * - une JFrame contenant l'arbre
+	 * - le résumé du modèle utilisé, comprenant la précision du modèle
 	 */
 	
 	public static void training(String patharff, String varY) throws Exception {
 		
-	     // J48 est la classe qui correspond à la classification des arbres CART
-	     J48 cls = new J48();
+	     // SMO est la classe qui gère les support vector machine
+	     SMO cls = new SMO();
 	     
 	     // on crée une instance, qui va lire le fichier .arff contenu dans l'adresse définie par patharff
 	     Instances data = new Instances(new BufferedReader(new FileReader(patharff)));
@@ -60,6 +56,8 @@ public class WekaTraining {
 	     // on crée les éch. d'app et de test
 	     Instances train = randData.trainCV(5, 0);
 	     Instances test = randData.testCV(5, 0);
+	     
+	     System.out.println("\n" + patharff + "\n");
 	     
 	     System.out.println("L'ensemble d'apprentissage dénombre " + train.numInstances() + " individus");
 	     System.out.println("L'ensemble de test dénombre " + test.numInstances() + " individus");     
@@ -81,25 +79,5 @@ public class WekaTraining {
 	     // Puis la précision
 	     System.out.println(eval.pctCorrect());
 	     
-	
-	     // Puis on cherche à afficher l'arbre obtenu dans une JFrame
-	     final JFrame jf = new JFrame("Weka Classifier Tree : " + patharff);
-	     
-	     // On définit les options de la fenêtre, puis on insère le graphique relatif à l'arbre CART
-	     jf.setSize(1800,1000);
-	     jf.getContentPane().setLayout(new BorderLayout());
-	     TreeVisualizer tv = new TreeVisualizer(null,cls.graph(),new PlaceNode2());
-	     jf.getContentPane().add(tv, BorderLayout.CENTER);
-	     
-	     // pour pouvoir fermer la fenêtre
-	     jf.addWindowListener(new java.awt.event.WindowAdapter() {
-	       public void windowClosing(java.awt.event.WindowEvent e) {
-	         jf.dispose();
-	       }
-	     });
-	     
-	     // enfin, on la rend visible
-	     jf.setVisible(true);
-	     tv.fitToScreen();
-	  }
+	}
 }
