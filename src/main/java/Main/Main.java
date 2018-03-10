deb4d9bfbe925623e7c4d3197ea4a36401ee2453
coray <yoann.coray@bbox.fr>
2018-03-10 22:58:13 +0100

    com main

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,6 +2,12 @@ package Main;
 
 import java.io.Serializable;
 
+/**
+ * En lançant cette main, nous pouvons renseigner les données nécessaires au calcul et va donner l'accuracy.
+ * Sinon, dans chacun des autres packages, il y a une main pour tester directement la librairie concernée par le package
+ * 
+ * 
+ */
 @SuppressWarnings("serial")
 public class Main implements Serializable {
 
@@ -11,7 +17,7 @@ public class Main implements Serializable {
 		Console console = new Console();
 		console.inputMain();
 		
-		// On crée l libraryavec les données de l'utilisateur
+		// On crée la library avec les données de l'utilisateur
 		Library l = new Library(console.strAdresse, console.strVariable, console.propApp, console.lib, console.algo, console.nbTrees);
 		
 		// On affiche l'accuracy
